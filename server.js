2f0adcf38898d5ea1ba39e88a2012e3bda1c44bd
Abdullah Zia <abdullahkhanzia07@gmail.com>
2025-04-04 02:01:27 -0700

    ejs Setup

--- server.js
+++ server.js
@@ -3,9 +3,27 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+app.set("view engine", "ejs");
+
+
 app.get("/", (req, res) => {
-    res.send("Hello");
+    res.render('index');
 });
+
+app.get("/users/register", (req, res) => {
+    res.render("register");
+})
+
+
+app.get("/users/login", (req, res) => {
+    res.render("login");
+})
+
+
+app.get("/users/dashboard", (req, res) => {
+    res.render("dashboard", {User: "Abdullah"});
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${4000}`);
